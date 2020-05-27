8a77ee6734f9260cb7fd2cc37e2d0ad7f1538baf
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-05-27 14:56:18 +0200

    updates script.js

--- Challenge/script.js
+++ Challenge/script.js
@@ -8,23 +8,36 @@ document.getElementsByClassName("screen two")[0].style.backgroundColor = `rgb(${
 let dices = ["images/dice-1.svg", "images/dice-2.svg", "images/dice-3.svg", "images/dice-4.svg", "images/dice-5.svg", "images/dice-6.svg"]
 let result = []
 
-function rollDices(screen, callback) {
+function rollDices(screen) {
   let i = 0
+  let showWinner = true;
   let interval = setInterval(function() {
     if (i == 14) {
       clearInterval(interval)
-      callback(result)
+      document.getElementsByClassName(`points ${screen}`)[0].children[0].innerHTML = `Your result is ${result[0] + result[1] + result[2] + 3}!`
+      for (j = 0; j < document.getElementsByClassName("points").length; j++) {
+        if (document.getElementsByClassName("points")[j].children[0].innerHTML == "") {
+          showWinner = false
+        }
+        if (showWinner) {
+          if (parseInt(document.getElementById("result1").innertHTML) > parseInt(document.getElementById("result2").innertHTML)) {
+            document.getElementsByClassName("winner one")[0].style.display = true
+            document.getElementsByClassName("loser one")[0].style.display = false
+          } else {
+            document.getElementsByClassName("winner two")[0].style.display = true
+            document.getElementsByClassName("loser two")[0].style.display = false
+          }
+        }
+      }
     } else {
       i++
       result = [Math.floor(Math.random() * 6), Math.floor(Math.random() * 6), Math.floor(Math.random() * 6)]
-      document.getElementsByTagName("img")[0].src = dices[result[0]]
-      document.getElementsByTagName("img")[1].src = dices[result[1]]
-      document.getElementsByTagName("img")[2].src = dices[result[2]]
+      document.getElementsByClassName(`dice alpha ${screen}`)[0].children[0].src = dices[result[0]]
+      document.getElementsByClassName(`dice betta ${screen}`)[0].children[0].src = dices[result[1]]
+      document.getElementsByClassName(`dice gamma ${screen}`)[0].children[0].src = dices[result[2]]
     }
   }, 200)
 }
 
-rollDices("one", function(result) {
-  return result
-})
+rollDices("one")
 //alert(result)
