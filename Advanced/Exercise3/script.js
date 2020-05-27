14b3d13f078ec51d45d7f11b6e401734ca2c5d1c
Irati Larreina <irati_6@hotmail.com>
2020-05-27 11:29:23 +0200

    Finished advanced exercises

---
+++ Advanced/Exercise3/script.js
@@ -0,0 +1,5 @@
+red = Math.floor(Math.random() * 256)
+green = Math.floor(Math.random() * 256)
+blue = Math.floor(Math.random() * 256)
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
